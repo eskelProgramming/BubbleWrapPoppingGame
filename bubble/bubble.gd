6ab208cbe26cab699d1d7bf493764fbeea0cac4f
Emily Skelley <166662972+eskelProgramming@users.noreply.github.com>
2026-04-07 09:38:50 -0700

    Audio and popping implemented

--- bubble/bubble.gd
+++ bubble/bubble.gd
@@ -2,6 +2,8 @@ class_name Bubble
 extends Button
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _on_pressed() -> void:
 	animated_sprite_2d.play("popped")
+	audio_stream_player_2d.play()
