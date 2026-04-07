6d8e8c2b45ed089cfa8b4436c753994fbf02601f
Emily Skelley <166662972+eskelProgramming@users.noreply.github.com>
2026-04-07 10:07:21 -0700

    level manager implemented
    Implemented Main Menu
    Implemented Level, LevelData, and LevelManager

---
+++ levels/scripts/level.gd
@@ -0,0 +1,16 @@
+## The basic information required for a playable Level.
+##
+## This Script is attached to each level scene. Keeps track of basic info for 
+## each level. 
+class_name Level
+
+# inherits from node
+extends Node
+
+## The unique ID for the level. Set in the editor. 
+@export var level_id : int
+
+## Extra information is stored in [member LevelData].
+var level_data : LevelData
+func _ready() -> void:
+	level_data = LevelManager.get_level_data_by_id(level_id)
---
+++ main.gd
@@ -0,0 +1,19 @@
+## The main script. 
+##
+## Prepares all necessary scenes and [LevelData]s. 
+
+extends Node
+
+## An [Array] of [LevelData]. Set in editor. 
+@export var available_levels : Array[LevelData]
+
+## The 2DScene to be loaded when the game is launched. 
+@onready var _2d_scene: Node = self
+@onready var main_menu: CanvasLayer = $main_menu
+
+## Sets [member LevelManager.main_scene] to [member _2d_scene] and
+## [member LevelManager.levels] to [member available_levels].
+func _ready() -> void:
+	LevelManager.main_scene = _2d_scene
+	LevelManager.levels = available_levels
+	LevelManager.main_menu = main_menu
