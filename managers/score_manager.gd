74519f12432af9a25b4ef37fc11b2afc9706f6dd
Emily Skelley <166662972+eskelProgramming@users.noreply.github.com>
2026-05-17 13:22:06 -0700

    Shop can now be left

--- managers/score_manager.gd
+++ managers/score_manager.gd
@@ -2,9 +2,9 @@ extends Node
 
 var curr_combo: int = 0
 var max_combo: int = 0
-var total_score: int
-var views: int
-var max_views: int
+var total_score: int = 0
+var curr_views: int = 0
+var total_views: int = 0
 
 func calc_pop_score():
 	var score = (1 if curr_combo > 0 else 0) * curr_combo
@@ -12,7 +12,14 @@ func calc_pop_score():
 	print("score %s" % score)
 
 func calc_views():
-	views = total_score * max_combo * PopManager.pop_bonus() / 5.0
+	curr_views = total_score * max_combo * PopManager.pop_bonus() / 5.0
+	total_views += curr_views
 	print("max_combo %s" % max_combo)
 	print("total_score %s" % total_score)
-	print("views %s" % views)
+	print("views %s" % curr_views)
+	print("total_views %s" % total_views)
+	
+func reset_scores():
+	curr_views = 0
+	curr_combo = 0
+	max_combo = 0
